ec5eadaebe282fee64e9152595133e5242671715
Fidel <genesfidelt@gmail.com>
2024-11-11 21:11:38 +0800

    update

--- models/userModel.js
+++ models/userModel.js
@@ -1,7 +1,7 @@
 const sqlite3 = require('sqlite3').verbose();
 
 // Open the SQLite database
-const db = new sqlite3.Database('./data/mydatabase.db', (err) => {
+const db = new sqlite3.Database('./mydatabase.db', (err) => {
   if (err) {
     console.error('Error opening database:', err.message);
   } else {
